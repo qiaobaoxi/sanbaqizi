df38f9e1f873ef0ee21df54eb03aadb3b3b7af13
乔宝喜 <1052913936@qq.com>
2018-04-08 16:42:14 +0800

    添加了3级联动

--- src/components/person-body.vue
+++ src/components/person-body.vue
@@ -5,16 +5,17 @@
       <span>姓名</span><input type="text" placeholder="请输入姓名" />
     </div>
     <div>
-      <span>手机</span><input type="text" placeholder="请输入姓名" />
+      <span>手机</span><input type="text" placeholder="请输入手机号" />
     </div>
     <div>
-      <span>验证码</span><input type="text" placeholder="请输入姓名" />
+      <span>验证码</span><input type="text" placeholder="请输入验证码" />
     </div>
     <div>
-      <span>地址</span><input type="text" placeholder="请输入姓名" />
+      <span>地址</span><v-distpicker class="dist-picker"></v-distpicker>
+      <div><input type="text" placeholder="请输入姓名" /> </div>
     </div>
     <div>
-      <span>预约日期</span><input type="text" placeholder="请输入姓名" />
+      <span>预约日期</span><input type="text" placeholder="请输入预约日期" />
     </div>
     <div>
       <span>预约时间</span><input type="text" placeholder="请输入姓名" />
@@ -33,6 +34,9 @@ export default {
   name: 'personbody',
   data () {
     return {
+        "province": "",
+        "city": "",
+        "area": ""
     }
   },
   components: {
@@ -42,5 +46,41 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang='less'>
-
+  .box{
+    padding: 0 .625rem;
+    >div{
+      margin-bottom: 1.125rem;
+      span{
+        display: inline-block;
+        margin-right: 0.5rem;
+        width: 5.5rem;
+        text-align: right;
+      }
+      input{
+        padding: 0.3rem 0.375rem;
+        width: 14rem;
+        height: 1rem;
+        outline:none;
+        border-radius: 0.375rem;
+        border: 1px solid #c5c5c7;
+      }
+      .dist-picker{
+        display:inline-block;
+        padding: 0;
+        width: 14rem;
+        height: 1.6rem;
+      }
+      .dist-picker /deep/ select{
+        padding: 0;
+        width: 4.25rem;
+        height: 100%;
+        font-size: 0.875rem;
+      }
+    }
+  }
+  h1 {
+    padding: 1.125rem 0;
+    text-align:center;
+    font-size: 1.125rem;
+  }
 </style>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import Distpicker from 'v-distpicker'
+Vue.component('v-distpicker', Distpicker)
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
@@ -10,5 +12,5 @@ new Vue({
   el: '#app',
   router,
   template: '<App/>',
-  components: { App }
+  components: {App}
 })
